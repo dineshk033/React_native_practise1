aaeb1dcde2abae4f80d4fa3dda06eb2bb90eaafa
dineshk033 <dineshk033@gmail.com>
2021-03-24 21:08:05 +0530

    Toolbar integration

--- App.js
+++ App.js
@@ -5,9 +5,16 @@ import {
   useFonts as useOswald,
   Oswald_400Regular,
 } from "@expo-google-fonts/oswald";
+import { Text } from "react-native";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
-import { theme } from "./src/features/globalTheme";
+import { Ionicons } from "@expo/vector-icons";
 import { DashBoardScreen } from "./src/features/dashboard/screen/dashboard.screen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import { theme } from "./src/features/globalTheme";
+const Tab = createBottomTabNavigator();
+const SettingsScreen = () => <Text>Setting</Text>;
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -24,9 +31,31 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <DashBoardScreen />
-        <ExpoStatusBar style="auto" />
+        <NavigationContainer>
+          <Tab.Navigator
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ color, size }) => {
+                let iconName;
+                if (route.name === "Home") {
+                  iconName = "md-home";
+                } else if (route.name === "Settings") {
+                  iconName = "md-settings";
+                }
+                // You can return any component that you like here!
+                return <Ionicons name={iconName} size={size} color={color} />;
+              },
+            })}
+            tabBarOptions={{
+              activeTintColor: "tomato",
+              inactiveTintColor: "grey",
+            }}
+          >
+            <Tab.Screen name="Home" component={DashBoardScreen} />
+            <Tab.Screen name="Settings" component={SettingsScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
       </ThemeProvider>
+      <ExpoStatusBar style="auto" />
     </>
   );
 }
